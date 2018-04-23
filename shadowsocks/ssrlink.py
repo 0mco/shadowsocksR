f03c20776b0ebba08c24a3d3a99e448af17547b0
Louis Xu <wickedbinary@gmail.com>
2018-04-23 15:19:46 +0800

    remove modules for testing.

--- shadowsocks/ssrlink.py
+++ shadowsocks/ssrlink.py
@@ -5,13 +5,6 @@
 Encode/decode SSR link.
 """
 
-# just using for test, remvoe it later.
-import os, sys
-if __name__ == '__main__':
-    import inspect
-    file_path = os.path.dirname(os.path.realpath(inspect.getfile(inspect.currentframe())))
-    sys.path.insert(0, os.path.join(file_path, '../'))
-
 from shadowsocks import common
 import base64
 
