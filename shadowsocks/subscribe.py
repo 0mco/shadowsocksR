ec5f18f40ebbfdf00fa86900cd2449fe98ae50e7
Louis Xu <wickedbinary@gmail.com>
2018-06-04 12:40:49 +0800

    validate ssr link in feed result

--- shadowsocks/subscribe.py
+++ shadowsocks/subscribe.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
 """
 Support for subscribtion.
 {'subscribe_addrs': {},
@@ -8,13 +7,19 @@ Support for subscribtion.
         'servers': {}}
 """
 
+if __name__ == '__main__':
+    import os, sys
+    file_path = os.path.dirname(os.path.realpath(__file__))
+    sys.path.insert(0, os.path.join(file_path, '../../'))
+    sys.path.insert(0, os.path.join(file_path, '../'))
+
 from shadowsocks.lib import ssrlink
 import requests
 import pickle
 import logging
 
 
-def retrive_ssrlink(url):
+def feed_ssr(url):
     """Retrive ssr links form url, return a list."""
     # TODO: sometimes we need to try to get via a proxy.
     base64_ssr = requests.get(url).text
@@ -22,7 +27,7 @@ def retrive_ssrlink(url):
     # The last one should be empty string.
     # Of course we can handle more correctly.
     # But as for now, it just works.
-    return ssrlinks[:-1]
+    return [link for link in ssrlinks if ssrlink.is_valide_ssrlink(link)]
 
 
 def load_servers():
@@ -33,7 +38,7 @@ def load_servers():
 
 def update_servers(url):
     try:
-        ssrlinks = retrive_ssrlink(url)
+        ssrlinks = feed_ssr(url)
         with open('ssr.pickle', 'w') as f:
             pickle.dump(ssrlinks, f)
     except Exception as e:
@@ -43,4 +48,4 @@ def update_servers(url):
 if __name__ == "__main__":
     import sys
     if len(sys.argv) == 2:
-        print(retrive_ssrlink(sys.argv[1]))
+        print(feed_ssr(sys.argv[1]))
