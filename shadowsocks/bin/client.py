d219525e179d8ad7f9daa5bc6e1737e51fdaddd5
Louis Xu <wickedbinary@gmail.com>
2018-06-04 12:39:17 +0800

    move main procedure to lib/service.py to create a Client class; remove inspect module

--- shadowsocks/bin/client.py
+++ shadowsocks/bin/client.py
@@ -18,21 +18,13 @@
 from __future__ import absolute_import, division, print_function, \
     with_statement
 
-import sys
-import os
-import logging
-import signal
-
 if __name__ == '__main__':
-    import inspect
-    file_path = os.path.dirname(
-        os.path.realpath(inspect.getfile(inspect.currentframe())))
+    import os, sys
+    file_path = os.path.dirname(os.path.realpath(__file__))
     sys.path.insert(0, os.path.join(file_path, '../../'))
 
 # NOTE: add '../../' to path if you want to execute directly.
-from shadowsocks.lib import shell
-from shadowsocks import daemon
-from shadowsocks.core import eventloop, tcprelay, udprelay, asyncdns
+from shadowsocks.lib import shell, service
 
 
 def main():
@@ -44,46 +36,8 @@ def main():
         p = os.path.dirname(os.path.abspath(sys.executable))
         os.chdir(p)
 
-    config = shell.get_config(True)
-
-    if not config.get('dns_ipv6', False):
-        asyncdns.IPV6_CONNECTION_SUPPORT = False
-
-    daemon.daemon_exec(config)
-    logging.info(
-        "local start with protocol [%s] password [%s] method [%s] obfs [%s] obfs_param [%s]"
-        % (config['protocol'], config['password'], config['method'],
-           config['obfs'], config['obfs_param']))
-
-    try:
-        logging.info("starting local at %s:%d" % (config['local_address'],
-                                                  config['local_port']))
-
-        dns_resolver = asyncdns.DNSResolver()
-        tcp_server = tcprelay.TCPRelay(config, dns_resolver, True)
-        udp_server = udprelay.UDPRelay(config, dns_resolver, True)
-        loop = eventloop.EventLoop()
-        dns_resolver.add_to_loop(loop)
-        tcp_server.add_to_loop(loop)
-        udp_server.add_to_loop(loop)
-
-        def handler(signum, _):
-            logging.warn('received SIGQUIT, doing graceful shutting down..')
-            tcp_server.close(next_tick=True)
-            udp_server.close(next_tick=True)
-
-        signal.signal(getattr(signal, 'SIGQUIT', signal.SIGTERM), handler)
-
-        def int_handler(signum, _):
-            sys.exit(1)
-
-        signal.signal(signal.SIGINT, int_handler)
-
-        daemon.set_user(config.get('user', None))
-        loop.run()
-    except Exception as e:
-        shell.print_exception(e)
-        sys.exit(1)
+    client = service.Client()
+    client.start()
 
 
 if __name__ == '__main__':
